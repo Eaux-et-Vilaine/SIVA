e19dd8e61f2e6938c0c02321b1e4d348f6f5e4fe
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-09-02 16:12:07 +0200

    dev : fix connexion problem

--- R/loaddb-tablesiva.R
+++ R/loaddb-tablesiva.R
@@ -1,11 +1,10 @@
-
 #' Generic function for loaddb
 #' @keywords internal
-setGeneric("loaddb",def=function(objet,...) standardGeneric("loaddb"))
+setGeneric("loaddb", def = function(objet, ...) standardGeneric("loaddb"))
 
 #' Methode loaddb
-#' 
-#' La méthode loaddb permet d'aller chercher les données 
+#'
+#' La méthode loaddb permet d'aller chercher les données
 #' @param objet, un objet de classe tablesiva
 #' @param con, une connexion pool
 #' @param checktag, faut il faire un check de l'existence d'un tag dans la table ?
@@ -18,12 +17,13 @@ setMethod(
   definition = function(objet, con, checktag = FALSE) {
     if (checktag) {
       # si il y a un @tag non NA, vérification qu'il est bien dans les tag de la table
-      if (!is.na(objet@tag)) {     
+      if (!is.na(objet@tag)) {
         sql <-
           paste0("SELECT distinct tag FROM archive_IAV.", objet@table)
-        tag <- DBI::dbGetQuery(conn=con,sql)
-        if (!objet@tag %in% tag$tag)
+        tag <- DBI::dbGetQuery(conn = con, sql)
+        if (!objet@tag %in% tag$tag) {
           stop("le tag n'existe pas dans la table")
+        }
       }
     }
     selectsiva <- stringr::str_c("SELECT horodate,valeur FROM archive_IAV.", objet@table)
@@ -39,30 +39,25 @@ setMethod(
         )
     } else {
       wheresiva <-
-        stringr::str_c(" WHERE horodate>'",
-                       objet@debut,
-                       "' AND horodate<'",
-                       objet@fin,
-                       "'")
+        stringr::str_c(
+          " WHERE horodate>'",
+          objet@debut,
+          "' AND horodate<'",
+          objet@fin,
+          "'"
+        )
     }
-    orderbysiva = " ORDER BY horodate"
+    orderbysiva <- " ORDER BY horodate"
     sql <- stringr::str_c(selectsiva, wheresiva, orderbysiva)
-    objet@rawdata <- DBI::dbGetQuery(conn=con, statement = sql)
-    cat(sprintf("Table %s(%s:%s), chargement de %s lignes \n",objet@nom,objet@table,objet@tag,nrow(objet@rawdata)))
+    objet@rawdata <- DBI::dbGetQuery(conn = con, statement = sql)
+    cat(sprintf("Table %s(%s:%s), chargement de %s lignes \n", objet@nom, objet@table, objet@tag, nrow(objet@rawdata)))
     # fix when using RmySQL instead of RMariaDB we have a problem the class reported is not the same (character instead of date)
-    if (attr(con$objClass,"package") == "RMySQL"){
-      objet@rawdata$horodate <- strptime(objet@rawdata$horodate, format ="%Y-%m-%d %H:%M:%S")
+    # This is necessary for RMySQL
+    objet@rawdata$horodate <- strptime(objet@rawdata$horodate, format = "%Y-%m-%d %H:%M:%S")
+    if (nrow(objet@rawdata) == 0) {
+      warning(paste("no data for", objet@table, "\n"))
     }
-    if (nrow(objet@rawdata) == 0) 
-      warning(paste("no data for", objet@table,"\n"))
     colnames(objet@rawdata)[2] <- objet@nom
     return(objet)
   }
 )
-
-
-
-
-
-
-
